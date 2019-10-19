cfc2b39f74675393a0fda698dc80d9380bbd435d
shanxuan <781244184@qq.com>
2019-10-19 16:16:36 +0800

    change name

--- local/gae/train.py
+++ local/gae/train.py
@@ -270,7 +270,8 @@ if __name__ == '__main__':
     # cur_metric, num_nodes, n_clusters = gae_for_na('heng_li')
     # print(cur_metric, num_nodes, n_clusters)
     # gae_for_na('heng_li', localTest=True)
-    gae_for_na('li_guo', localTest=True)
+    # gae_for_na('li_guo', localTest=True)
+    gae_for_na('hong_xiao', localTest=True)
     # main()
 
 
