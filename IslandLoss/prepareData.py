c4b805c2545372944bd175e8166c1133e9709ec5
shanxuan <781244184@qq.com>
2019-10-21 14:27:33 +0800

    Island Loss

--- IslandLoss/prepareData.py
+++ IslandLoss/prepareData.py
@@ -4,6 +4,10 @@ import codecs
 from os.path import join
 from datetime import datetime
 from utils import feature_utils
+from global_.embedding import EmbeddingModel
+from utils.cache import LMDBClient
+import math
+
 
 start_time = datetime.now()
 
@@ -25,7 +29,80 @@ def dump_paper_feature_to_file():
         wf.write(pid + '\t' + ' '.join(author_feature) + '\n')
     wf.close()
 
+def dump_author_features_to_cache():
+    """
+    dump author features to lmdb
+    """
+    LMDB_NAME = 'publication_IslandLoss.feature'
+    lc = LMDBClient(LMDB_NAME)
+    with codecs.open(join(settings.ISLAND_LOSS_DIR, 'paper_features.txt'), 'r', encoding='utf-8') as rf:
+        for i, line in enumerate(rf):
+            if i % 1000 == 0:
+                print('line', i)
+            items = line.rstrip().split('\t')
+            pid_order = items[0]
+            # print ("pid_order: ", pid_order, items)
+            author_features = items[1].split()
+            lc.set(pid_order, author_features)
+
+
+def cal_feature_idf():
+    """
+    calculate word IDF (Inverse document frequency) using publication data
+    """
+    feature_dir = join(settings.DATA_DIR, 'IslandLoss')
+    counter = dd(int)
+    cnt = 0
+    LMDB_NAME = 'publication_IslandLoss.feature'
+    lc = LMDBClient(LMDB_NAME)
+    author_cnt = 0
+    with lc.db.begin() as txn:
+        for k in txn.cursor():
+            features = data_utils.deserialize_embedding(k[1])
+            if author_cnt % 10000 == 0:
+                print(author_cnt, features[0], counter.get(features[0]))
+            author_cnt += 1
+            for f in features:
+                cnt += 1
+                counter[f] += 1
+    idf = {}
+    for k in counter:
+        idf[k] = math.log(cnt / counter[k])
+    data_utils.dump_data(dict(idf), feature_dir, "IslandLoss_feature_idf.pkl")
+
+
+def dump_author_embs():
+    """
+    dump author embedding to lmdb
+    author embedding is calculated by weighted-average of word vectors with IDF
+    """
+    emb_model = EmbeddingModel.Instance()
+    idf = data_utils.load_data(settings.ISLAND_LOSS_DIR, 'IslandLoss_feature_idf.pkl')
+    print('idf loaded')
+    LMDB_NAME_FEATURE = 'publication_IslandLoss.feature'
+    lc_feature = LMDBClient(LMDB_NAME_FEATURE)
+    LMDB_NAME_EMB = "publication.emb.weighted"
+    lc_emb = LMDBClient(LMDB_NAME_EMB)
+    cnt = 0
+    with lc_feature.db.begin() as txn:
+        for k in txn.cursor():
+            if cnt % 1000 == 0:
+                print('cnt', cnt, datetime.now()-start_time)
+            cnt += 1
+            pid = k[0].decode('utf-8')
+            # print ("pid_order: ", pid_order)
+            features = data_utils.deserialize_embedding(k[1])
+            cur_emb = emb_model.project_embedding(features, idf)
+            if cur_emb is not None:
+                # print ("pid_order: is not none", pid_order)
+                lc_emb.set(pid, cur_emb)
+
+
 
 if __name__ == '__main__':
     dump_paper_feature_to_file()
-
+    dump_author_features_to_cache()
+    emb_model = EmbeddingModel.Instance()
+    emb_model.train('shanxuan_islandLoss', LMDB_NAME='publication_IslandLoss.feature')  # training word embedding model
+    cal_feature_idf()
+    dump_author_embs()
