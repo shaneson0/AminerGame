77aa2cf52d7dcddd1bb14f022627a931be653883
shanxuan <781244184@qq.com>
2019-10-21 16:34:57 +0800

    prepare Train Data

--- IslandLoss/prepareTrainData.py
+++ IslandLoss/prepareTrainData.py
@@ -10,19 +10,42 @@ from collections import defaultdict as dd
 from sklearn.model_selection import train_test_split
 import math
 import numpy as np
+import json
 
+
+# prepare Data
 TrainPids = []
-train_author = data_utils.load_data(settings.TRAIN_PUB_DIR, 'train_author.json')
+with open(join(settings.TRAIN_PUB_DIR, "train_author.json"), "r") as fp:
+    train_author = json.load(fp)
+    fp.close()
 for name in train_author.keys():
     for aid in train_author[name]:
-        for pids in train_author[name][aid]:
-            TrainPids = TrainPids + pids
+        TrainPids = TrainPids + train_author[name][aid]
 
 TrainPids = np.array(TrainPids)
 
 TrainPids, TestPids = train_test_split(TrainPids, test_size=0.33, random_state=42)
+PUBLICATION_LABEL = 'Publication.label'
+lc_publication_label = LMDBClient(PUBLICATION_LABEL)
+
+LMDB_NAME_EMB = "publication.emb.weighted"
+lc_emb = LMDBClient(LMDB_NAME_EMB)
+
+TrainX = []
+TrainY = []
+
+
+for pid in TrainPids:
+    emb = lc_emb.get(pid)
+    label = lc_emb.get(pid)
+    print ("pid: ", pid, ", label: ", label)
+    if emb is None:
+        continue
+    TrainX.append(emb)
+    TrainY.append()
+
 print (TrainPids, len(TrainPids))
-print (TestPids, len(TrainPids))
+print (TestPids, len(TestPids))
 
 
 
