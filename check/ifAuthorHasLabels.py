3c50bbc179393a32a36c7426f3146232b11dda04
shanxuan <781244184@qq.com>
2019-10-21 12:11:19 +0800

    check triplet loss embedding

--- check/ifAuthorHasLabels.py
+++ check/ifAuthorHasLabels.py
@@ -6,14 +6,18 @@ TrainAuthor = load_json(settings.TRAIN_PUB_DIR, 'train_author.json')
 TrainPub = load_json(settings.TRAIN_PUB_DIR, 'train_pub.json')
 
 
+AllPaperNumber = 0
 cnt = 0
 for name in TrainAuthor.keys():
     Author = TrainAuthor[name]
     for pid in Author.keys():
        if pid not in TrainPub:
-           print ("%s not in TrainPub")
+           print ("%s not in TrainPub"%(pid))
            cnt += 1
 
+       AllPaperNumber += 1
+
 print ("total number of paper not in TrainPub is: %d"%(cnt))
+print ("total number paper is: %d"%(AllPaperNumber))
 
 
