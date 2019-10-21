1b539810a2ab81bbb3af10c24c24097556618659
shanxuan <781244184@qq.com>
2019-10-21 16:06:00 +0800

    Train Data

--- IslandLoss/prepareTrainData.py
+++ IslandLoss/prepareTrainData.py
@@ -7,15 +7,22 @@ from utils import feature_utils
 from global_.embedding import EmbeddingModel
 from utils.cache import LMDBClient
 from collections import defaultdict as dd
-
+from sklearn.model_selection import train_test_split
 import math
+import numpy as np
 
-
-
+TrainPids = []
 train_author = data_utils.load_data(settings.TRAIN_PUB_DIR, 'train_author.json')
 for name in train_author.keys():
-    for pid in train_author[name]:
+    for aid in train_author[name]:
+        for pids in train_author[name][aid]:
+            TrainPids = TrainPids + pids
 
+TrainPids = np.array(TrainPids)
+
+TrainPids, TestPids = train_test_split(TrainPids, test_size=0.33, random_state=42)
+print (TrainPids, len(TrainPids))
+print (TestPids, len(TrainPids))
 
 
 
