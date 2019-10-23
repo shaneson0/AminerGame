d5a5a931337ef0ff0bb77210dec6d42396939a4b
shanxuan <781244184@qq.com>
2019-10-23 15:15:14 +0800

    add softmax

--- IslandLoss/IslandLossModel.py
+++ IslandLoss/IslandLossModel.py
@@ -75,7 +75,7 @@ class CenterLossModel(object):
         return encoded_emb
 
     def buildOptimizer(self, encoded_emb):
-        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=encoded_emb, targets=self.placeholder['labels']))
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=encoded_emb, labels=self.placeholder['labels']))
         centerloss, centers, centers_update_op = self.get_center_loss(encoded_emb, self.placeholder['labels'], self.alpha, self.num_classes)
         loss = loss + centerloss
 
