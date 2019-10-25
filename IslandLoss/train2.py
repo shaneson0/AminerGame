2398eb4fc0b0864b2eea732825f941f7f9d57386
shanxuan <781244184@qq.com>
2019-10-26 03:18:59 +0800

    调参

--- IslandLoss/train2.py
+++ IslandLoss/train2.py
@@ -72,8 +72,8 @@ print ("pass")
 
 Embedding = 100
 NUM_CLASSES = NumberOfClass
-CENTER_LOSS_ALPHA = 1.0
-Island_Loss_ALPHA = 1.0
+CENTER_LOSS_ALPHA = 0.5
+Island_Loss_ALPHA = 0.5
 ratio = 0.0001
 epochs = 3000
 
