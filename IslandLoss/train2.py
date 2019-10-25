54a7296bfedcf875d09e65d19e4326e8e5501eef
shanxuan <781244184@qq.com>
2019-10-25 13:19:37 +0800

    加上2范正则

--- IslandLoss/train2.py
+++ IslandLoss/train2.py
@@ -82,13 +82,11 @@ global_step = tf.Variable(0, trainable=False, name='global_step')
 layer1 = tf.keras.layers.Dense(100, activation='relu', name='first_emb_layer')(input_images)
 layer1 = tf.keras.layers.Dropout(0.9)(layer1)
 
-layer2 = tf.keras.layers.Dense(64, activation='relu', name='last_emb_layer')(layer1)
+layer2 = tf.keras.layers.Dense(64, activation='relu', name='last_emb_layer', kernel_regularizer=tf.keras.regularizers.l2(0.1))(layer1)
 layer2 = tf.keras.layers.Dropout(0.9)(layer2)
 
-layer3 = tf.keras.layers.Dense(32, activation='relu', name='last_emb_layer')(layer2)
-layer3 = tf.keras.layers.Dropout(0.9)(layer3)
 
-feature = tf.keras.layers.Lambda(l2Norm, name='norm_layer', output_shape=[32])(layer3)
+feature = tf.keras.layers.Lambda(l2Norm, name='norm_layer', output_shape=[32])(layer2)
 logits = tf.keras.layers.Dense(NUM_CLASSES, activation='softmax', kernel_regularizer=tf.keras.regularizers.l2(0.1))(feature)
 
 with tf.name_scope('loss'):
