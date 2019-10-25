734b7e2ab817a5464fab0614cb8fe9dcd26cd9a9
shanxuan <781244184@qq.com>
2019-10-26 02:39:38 +0800

    调参

--- IslandLoss/train2.py
+++ IslandLoss/train2.py
@@ -72,9 +72,9 @@ print ("pass")
 
 Embedding = 100
 NUM_CLASSES = NumberOfClass
-CENTER_LOSS_ALPHA = 0.05
-Island_Loss_ALPHA = 0.05
-ratio = 1.0
+CENTER_LOSS_ALPHA = 0.0001
+Island_Loss_ALPHA = 1.0
+ratio = 0.0001
 epochs = 3000
 
 with tf.name_scope('input'):
