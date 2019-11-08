547002f828760aedb93827fa47347c60f7509c1b
shanxuan <781244184@qq.com>
2019-11-08 15:33:02 +0800

    cluster number

--- cluster_size/count.py
+++ cluster_size/count.py
@@ -102,7 +102,7 @@ def gen_test(k=300, flatten=False):
         items = []
         for c in name_to_pubs_test[name]:  # one person
             for item in name_to_pubs_test[name][c]:
-                if data_cache[item] is None or lc.get(pid) is None:
+                if  item not in data_cache  or lc.get(item) is None:
                     continue
                 items.append(item)
         sampled_points = [items[p] for p in np.random.choice(len(items), k, replace=True)]
