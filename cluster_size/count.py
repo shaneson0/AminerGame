d0b33678f933c18f8600f09ac0a6a129643b6b9c
shanxuan <781244184@qq.com>
2019-11-08 15:43:40 +0800

    cluster number

--- cluster_size/count.py
+++ cluster_size/count.py
@@ -105,6 +105,9 @@ def gen_test(k=300, flatten=False):
                 if  item not in data_cache  or lc.get(item) is None:
                     continue
                 items.append(item)
+        if len(items) == 0:
+            continue
+
         sampled_points = [items[p] for p in np.random.choice(len(items), k, replace=True)]
         for p in sampled_points:
             if p in data_cache:
