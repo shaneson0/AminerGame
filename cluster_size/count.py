2b9519e51ca9cf4f4ba401abeb70fa70b58f4d5e
shanxuan <781244184@qq.com>
2019-11-08 15:27:16 +0800

    cluster number

--- cluster_size/count.py
+++ cluster_size/count.py
@@ -79,6 +79,9 @@ def gen_sna(k=300):
             if lc.get(pid) is not None:
                 items.append(pid)
 
+        if len(items) == 0:
+            continue
+
         sampled_points = [items[p] for p in np.random.choice(len(items), k, replace=True)]
         for p in sampled_points:
             emb = lc.get(p)
